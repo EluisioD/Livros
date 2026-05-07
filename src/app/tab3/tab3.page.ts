d71fe2f14225df15e40a163acab65b56833b71f0
Eluisio Dias <eluisiod@gmail.com>
2026-05-07 17:02:10 +0100

    Corrigir para 2.º ano e adicionar instruções para foto de perfil

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -19,7 +19,10 @@ export class Tab3Page {
     dataNascimento: '24 de fevereiro de 2005',
     naturalidade: 'Meaux, França',
     idade: 19,
-    foto: 'https://ionicframework.com/docs/img/demos/avatar.png', // Foto de perfil
+    // PARA ADICIONAR A TUA FOTO:
+    // Opção 1: Coloca a tua foto em src/assets/ e usa: 'assets/nome-do-ficheiro.jpg'
+    // Opção 2: Usa um URL online: 'https://www.exemplo.com/foto.jpg'
+    foto: 'https://ionicframework.com/docs/img/demos/avatar.png', // Substitui este URL pela tua foto
     // Biografia do autor com informações pessoais e académicas
     biografia: 'Eluisio Dias nasceu a 24 de fevereiro de 2005 em Meaux. Atualmente, frequenta o 2.º ano de Engenharia Informática no Instituto Politécnico de Viana do Castelo, na Escola Superior de Tecnologia e Gestão.\n\nAlém do percurso académico, destaca-se pelo forte interesse em atletismo, sendo estudante-atleta e procurando constantemente melhorar o seu desempenho desportivo, especialmente nas provas de velocidade. Tem também interesse por liderança, trabalho em equipa e desenvolvimento pessoal, participando em formações como a Academia de Liderança Colaborativa.\n\nNa área tecnológica e criativa, demonstra entusiasmo por programação, criação de websites, desenvolvimento de projetos em Python e produção de conteúdos digitais. Tem interesse em videojogos, modelação 3D, tecnologia e criação de vídeos para o YouTube, incluindo gameplays e conteúdos relacionados com setups e coleções pessoais.\n\nFora da tecnologia e do desporto, gosta de imaginar projetos criativos, construir modelos LEGO originais e explorar novas ideias para o futuro.',
     // Array de interesses e hobbies
---
+++ src/assets/README_FOTO_PERFIL.txt
@@ -0,0 +1,25 @@
+<!-- INSTRUÇÕES PARA ADICIONAR A FOTO DE PERFIL -->
+
+1. PARA ADICIONAR A TUA FOTO:
+   - Coloca a tua foto (ex: "perfil.jpg" ou "perfil.png") na pasta: src/assets/
+   - Depois edita o ficheiro tab3.page.ts
+   - Na linha onde diz: foto: 'assets/perfil.png',
+   - Substitui "perfil.png" pelo nome do teu ficheiro
+
+2. EXEMPLO:
+   Se o teu ficheiro se chama "minha-foto.jpg":
+   foto: 'assets/minha-foto.jpg',
+
+3. FORMATOS ACEITES:
+   - .jpg / .jpeg
+   - .png
+   - .gif
+   - .webp
+
+4. TAMANHO RECOMENDADO:
+   - Quadrada (ex: 400x400px ou 500x500px)
+   - Menos de 1MB
+
+5. ALTERNATIVA - USAR URL ONLINE:
+   Se preferires usar uma foto online (ex: do Instagram), podes usar:
+   foto: 'https://www.exemplo.com/minha-foto.jpg',
